df25161e746378bd13d89e058bd7a7bd01b75de4
DeadMorozz <evlampejev@gmail.com>
2020-11-24 02:52:30 +0400

    custom

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,17 +1,200 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <admin-view :quiz-data="quizData" v-if="user.isAdmin" :user-obj="user"></admin-view>
+    <user-view :quiz-data="quizData" :user-obj="user" v-else></user-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AdminView from "./components/AdminView";
+import UserView from "./components/UserView";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'admin-view': AdminView,
+    'user-view': UserView
+  },
+  data() {
+    return {
+      user: {
+        name: 'MegaUser',
+        isAdmin: true
+      },
+      tabVisible: true,
+      quizData: [
+        //     {
+        //   date: '2016-05-03',
+        //   name: 'test #1',
+        //   author: 'author #1',
+        //   length: '10',
+        //   isTimeLimited: false,
+        //   timeLimit: 'неограничено',
+        //   questions: [
+        //     {
+        //       id: 0,
+        //       qText: 'быть или не быть?',
+        //       type: 0,
+        //       answers: [{
+        //         id: 0,
+        //         questionId: 0,
+        //         aText: 'быть',
+        //         correct: true
+        //       }, {
+        //         id: 1,
+        //         questionId: 0,
+        //         aText: 'не быть',
+        //         correct: false
+        //       }, {
+        //         id: 2,
+        //         questionId: 0,
+        //         aText: 'где я?',
+        //         correct: false
+        //       }
+        //       ]
+        //     }, {
+        //       id: 1,
+        //       qText: 'Кто там?',
+        //       type: 1,
+        //       answers: [{
+        //         id: 3,
+        //         questionId: 1,
+        //         aText: 'почтальон печкин',
+        //         correct: true
+        //       }, {
+        //         id: 4,
+        //         questionId: 1,
+        //         aText: 'конь в пальто',
+        //         correct: false
+        //       }
+        //       ]
+        //     }
+        //   ]
+        // }, {
+        //   date: '2016-05-02',
+        //   name: 'test #2',
+        //   author: 'author #2',
+        //   length: '4',
+        //   questions: [{
+        //     id: 0,
+        //     qText: 'быть или не быть?',
+        //     type: 0,
+        //     answers: [{
+        //       id: 0,
+        //       questionId: 0,
+        //       aText: 'быть',
+        //       correct: true
+        //     }, {
+        //       id: 1,
+        //       questionId: 0,
+        //       aText: 'не быть',
+        //       correct: false
+        //     }, {
+        //       id: 2,
+        //       questionId: 0,
+        //       aText: 'где я?',
+        //       correct: false
+        //     }
+        //     ]
+        //   }, {
+        //     id: 1,
+        //     qText: 'Кто там?',
+        //     type: 1,
+        //     answers: [{
+        //       id: 3,
+        //       questionId: 1,
+        //       aText: 'почтальон печкин',
+        //       correct: true
+        //     }, {
+        //       id: 4,
+        //       questionId: 1,
+        //       aText: 'конь в пальто',
+        //       correct: false
+        //     }
+        //     ]
+        //   }]
+        // }, {
+        //   date: '2016-05-04',
+        //   name: 'test #3',
+        //   author: 'author #3',
+        //   length: '6',
+        //   questions: [{
+        //     id: 0,
+        //     qText: 'быть или не быть?',
+        //     type: 0,
+        //     answers: [{
+        //       id: 0,
+        //       questionId: 0,
+        //       aText: 'быть',
+        //       correct: true
+        //     }, {
+        //       id: 1,
+        //       questionId: 0,
+        //       aText: 'не быть',
+        //       correct: false
+        //     }, {
+        //       id: 2,
+        //       questionId: 0,
+        //       aText: 'где я?',
+        //       correct: false
+        //     }
+        //     ]
+        //   }, {
+        //     id: 1,
+        //     qText: 'Кто там?',
+        //     type: 1,
+        //     answers: [{
+        //       id: 3,
+        //       questionId: 1,
+        //       aText: 'почтальон печкин',
+        //       correct: true
+        //     }, {
+        //       id: 4,
+        //       questionId: 1,
+        //       aText: 'конь в пальто',
+        //       correct: false
+        //     }
+        //     ]
+        //   }]
+        // }, {
+        //   date: '2016-05-01',
+        //   name: 'test #4',
+        //   author: 'author #4',
+        //   length: '9',
+        //   questions: []
+        // }, {
+        //   date: '2016-05-02',
+        //   name: 'test #2',
+        //   author: 'author #2',
+        //   length: '4',
+        //   questions: []
+        // }, {
+        //   date: '2016-05-04',
+        //   name: 'test #3',
+        //   author: 'author #3',
+        //   length: '6',
+        //   questions: []
+        // }
+      ]
+    }
+  },
+  methods: {
+    getQuizes() {
+
+    }
+  },
+  mounted: function () {
+
+    this.$api.quiz.getAllQuiz().then(result => {
+      if (result.status === 200 || result.status === 201) {
+        console.log(result.data);
+        this.quizData = result.data;
+      } else {
+        this.errors = result;
+        console.error(result);
+      }
+    });
+
   }
 }
 </script>
@@ -22,7 +205,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2f5b98;
+}
+
+body {
+  height: 100%;
 }
 </style>
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/ru-RU'
 import App from './App.vue'
-import store from './store'
+import quizApi from "./plugins/api";
+import AxiosPlugin from 'vue-axios-cors';
+import vuex from 'vuex'
+import '@babel/polyfill'
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI, {locale});
+Vue.use(AxiosPlugin)
+Vue.use(quizApi)
+Vue.use(vuex)
+
+Vue.directive('focus', {
+    inserted: function (el) {
+        el.focus()
+    }
+})
+
 new Vue({
-  store,
-  render: h => h(App)
+    render: h => h(App),
 }).$mount('#app')
